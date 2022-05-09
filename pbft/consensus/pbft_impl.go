c261e4aca424cfdd27236f9d8c932830b0988fab
赵云潇 <zhao.yunxiao@outlook.com>
2022-05-09 17:58:40 +0800

    .

--- pbft/consensus/pbft_impl.go
+++ pbft/consensus/pbft_impl.go
@@ -3,8 +3,8 @@ package consensus
 import (
 	"encoding/json"
 	"errors"
-	"time"
 	"fmt"
+	"time"
 )
 
 type State struct {
@@ -15,114 +15,116 @@ type State struct {
 }
 
 type MsgLogs struct {
-	ReqMsg        *RequestMsg
-	PrepareMsgs   map[string]*VoteMsg
-	CommitMsgs    map[string]*VoteMsg
+	ReqMsg      *RequestMsg
+	PrepareMsgs map[string]*VoteMsg
+	CommitMsgs  map[string]*VoteMsg
 }
 
 type Stage int
+
 const (
-	Idle        Stage = iota // Node is created successfully, but the consensus process is not started yet.
-	PrePrepared              // The ReqMsgs is processed successfully. The node is ready to head to the Prepare stage.
-	Prepared                 // Same with `prepared` stage explained in the original paper.
-	Committed                // Same with `committed-local` stage explained in the original paper.
+	Idle        Stage = iota // 节点被成功创建，但共识还没有开始
+	PrePrepared              // Request 被成功处理，但是还没有到达prepare阶段
+	Prepared                 // 原论文中的 prepared 状态
+	Committed                // 原论文中的 committed-local 状态
 )
 
-// f: # of Byzantine faulty node
-// f = (n­1) / 3
-// n = 4, in this case.
-const f = 1
+// f: 拜占庭错误节点的数量
+// f = (n - ­1) / 3
+// 本例中n = 7.
+const f = 2
 
-// lastSequenceID will be -1 if there is no last sequence ID.
+// 如果不存在 lastSequenceID，则赋值为-1
 func CreateState(viewID int64, lastSequenceID int64) *State {
 	return &State{
 		ViewID: viewID,
 		MsgLogs: &MsgLogs{
-			ReqMsg:nil,
-			PrepareMsgs:make(map[string]*VoteMsg),
-			CommitMsgs:make(map[string]*VoteMsg),
+			ReqMsg:      nil,
+			PrepareMsgs: make(map[string]*VoteMsg),
+			CommitMsgs:  make(map[string]*VoteMsg),
 		},
 		LastSequenceID: lastSequenceID,
-		CurrentStage: Idle,
+		CurrentStage:   Idle,
 	}
 }
 
 func (state *State) StartConsensus(request *RequestMsg) (*PrePrepareMsg, error) {
-	// `sequenceID` will be the index of this message.
+	// 用时间戳设置唯一的 sequenceID
 	sequenceID := time.Now().UnixNano()
-
-	// Find the unique and largest number for the sequence ID
 	if state.LastSequenceID != -1 {
 		for state.LastSequenceID >= sequenceID {
 			sequenceID += 1
 		}
 	}
-
-	// Assign a new sequence ID to the request message object.
 	request.SequenceID = sequenceID
 
-	// Save ReqMsgs to its logs.
+	// 将request保存至 MsgLogs
 	state.MsgLogs.ReqMsg = request
 
-	// Get the digest of the request message
+	// 杂凑 request 内容
 	digest, err := digest(request)
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
 
-	// Change the stage to pre-prepared.
+	// 将状态更改为 prepared
 	state.CurrentStage = PrePrepared
 
+	// 新建并返回 PrePrepareMsg，用于 PBFT 下一阶段
 	return &PrePrepareMsg{
-		ViewID: state.ViewID,
+		ViewID:     state.ViewID,
 		SequenceID: sequenceID,
-		Digest: digest,
+		Digest:     digest,
 		RequestMsg: request,
 	}, nil
 }
 
 func (state *State) PrePrepare(prePrepareMsg *PrePrepareMsg) (*VoteMsg, error) {
-	// Get ReqMsgs and save it to its logs like the primary.
+	// 将 prePrepareMsg 中的 RequestMsg 保存到 MsgLogs 中
 	state.MsgLogs.ReqMsg = prePrepareMsg.RequestMsg
 
-	// Verify if v, n(a.k.a. sequenceID), d are correct.
+	// 检查 prePrepareMsg 各项字段是否正确
 	if !state.verifyMsg(prePrepareMsg.ViewID, prePrepareMsg.SequenceID, prePrepareMsg.Digest) {
 		return nil, errors.New("pre-prepare message is corrupted")
 	}
 
-	// Change the stage to pre-prepared.
+	// 将状态更改为 PrePrepared
 	state.CurrentStage = PrePrepared
 
+	// 新建并返回 prepareMsg，用于 PBFT 下一阶段
+	// 如果某个从结点验证通过了某条 PrePrepared，那么它将进入 prepare 阶段
+	// 故此处无需检查是否收到超过 2f 个节点的 prePrepareMsg
 	return &VoteMsg{
-		ViewID: state.ViewID,
+		ViewID:     state.ViewID,
 		SequenceID: prePrepareMsg.SequenceID,
-		Digest: prePrepareMsg.Digest,
-		MsgType: PrepareMsg,
+		Digest:     prePrepareMsg.Digest,
+		MsgType:    PrepareMsg,
 	}, nil
 }
 
-
-func (state *State) Prepare(prepareMsg *VoteMsg) (*VoteMsg, error){
+func (state *State) Prepare(prepareMsg *VoteMsg) (*VoteMsg, error) {
 	if !state.verifyMsg(prepareMsg.ViewID, prepareMsg.SequenceID, prepareMsg.Digest) {
 		return nil, errors.New("prepare message is corrupted")
 	}
 
-	// Append msg to its logs
+	// 将 PrepareMsgs 保存到 log 中
 	state.MsgLogs.PrepareMsgs[prepareMsg.NodeID] = prepareMsg
 
-	// Print current voting status
+	// 打印当前投票情况
 	fmt.Printf("[Prepare-Vote]: %d\n", len(state.MsgLogs.PrepareMsgs))
 
+	// 如果收到超过 2f 个节点的 PrepareMsgs
 	if state.prepared() {
-		// Change the stage to prepared.
+		// 将状态更改为 Prepared
 		state.CurrentStage = Prepared
 
+		// 新建并返回 CommitMsg，用于 PBFT 下一阶段
 		return &VoteMsg{
-			ViewID: state.ViewID,
+			ViewID:     state.ViewID,
 			SequenceID: prepareMsg.SequenceID,
-			Digest: prepareMsg.Digest,
-			MsgType: CommitMsg,
+			Digest:     prepareMsg.Digest,
+			MsgType:    CommitMsg,
 		}, nil
 	}
 
@@ -134,29 +136,33 @@ func (state *State) Commit(commitMsg *VoteMsg) (*ReplyMsg, *RequestMsg, error) {
 		return nil, nil, errors.New("commit message is corrupted")
 	}
 
-	// Append msg to its logs
+	// 将 commitMsg 保存到 log 中
 	state.MsgLogs.CommitMsgs[commitMsg.NodeID] = commitMsg
 
-	// Print current voting status
+	// 打印当前投票情况
 	fmt.Printf("[Commit-Vote]: %d\n", len(state.MsgLogs.CommitMsgs))
 
+	// 如果收到超过 2f 个节点的 commitMsg
 	if state.committed() {
-		// This node executes the requested operation locally and gets the result.
+		// 本地执行请求并得到结果
 		result := "Executed"
 
-		// Change the stage to prepared.
+		// 将状态更改为 Committed
 		state.CurrentStage = Committed
 
+		// 返回 ReplyMsg 和 ReqMsg
 		return &ReplyMsg{
-			ViewID: state.ViewID,
+			ViewID:    state.ViewID,
 			Timestamp: state.MsgLogs.ReqMsg.Timestamp,
-			ClientID: state.MsgLogs.ReqMsg.ClientID,
-			Result: result,
+			ClientID:  state.MsgLogs.ReqMsg.ClientID,
+			Result:    result,
 		}, state.MsgLogs.ReqMsg, nil
 	}
 
 	return nil, nil, nil
 }
+
+//---------工具函数---------
 
 func (state *State) verifyMsg(viewID int64, sequenceID int64, digestGot string) bool {
 	// Wrong view. That is, wrong configurations of peers to start the consensus.
